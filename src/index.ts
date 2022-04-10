1d0194a8b5e139bd25eda249922033fd5e50a23a
Davide Tantillo <davide.tantillo@mia-platform.eu>
2022-04-10 14:33:53 +0200

    First commit

---
+++ src/index.ts
@@ -0,0 +1,20 @@
+import { TodoItem } from "./todoItem"
+import { TodoCollection } from "./todoCollection"
+
+let todos: TodoItem[] = [
+    new TodoItem(1, "Buy Flowers for Erika"),
+    new TodoItem(2, "Get Shoes"),
+    new TodoItem(3, "Collect Tickets"),
+    new TodoItem(4, "Call Scuolapercani"),
+]
+
+let collection: TodoCollection = new TodoCollection("Davide", todos)
+
+console.clear()
+console.log(`${collection.userName}'s Todo List`)
+
+let newId: number = collection.addTodo("Go for run")
+let todoItem: TodoItem = collection.getTodoById(newId)
+todoItem.printDetails()
+
+// collection.addTodo(todoItem)
