4e003d233814200df4234e27132d62c7a1594237
Davide Tantillo <davide.tantillo@mia-platform.eu>
2022-04-15 22:22:08 +0200

    changed target to es5, introduced non supported elements such as Map and re-added the support through the lib property

--- tools/dist/index.js
+++ tools/dist/index.js
@@ -1,6 +1,10 @@
 function printMessage(msg) {
-    console.log("Message: " + msg);
+    console.log("Message: ".concat(msg));
 }
 var message = "Hello, TypeScript";
 printMessage(message);
 printMessage("It is sunny today");
+var data = new Map();
+data.set("Bob", "London");
+data.set("Alice", "Paris");
+data.forEach(function (val, key) { return console.log("".concat(key, " lives in ").concat(val)); });
--- tools/src/index.ts
+++ tools/src/index.ts
@@ -6,3 +6,8 @@ let message = "Hello, TypeScript"
 
 printMessage(message)
 printMessage("It is sunny today")
+
+let data = new Map();
+data.set("Bob", "London");
+data.set("Alice", "Paris");
+data.forEach((val, key) => console.log(`${key} lives in ${val}`));
