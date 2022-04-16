5a01a8e46492d19268fb2cd92865d6d0b7bd4641
Davide Tantillo <davide.tantillo@mia-platform.eu>
2022-04-16 11:30:39 +0200

    Show implicit defined static type

--- types/src/index.ts
+++ types/src/index.ts
@@ -5,5 +5,5 @@ function calculateTax(amount: number): number {
 const price: number = 12
 console.log(`No TAX price: ${price} - TAX Price: ${calculateTax(price)}`)
 
-const stringPrice: string = "12"
+const stringPrice = "12"
 console.log(`No TAX price: ${stringPrice} - TAX stringPrice: ${calculateTax(stringPrice)}`)
