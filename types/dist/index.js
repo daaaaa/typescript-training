75f7bf6c61ed7bbe94ba79b5fa88667da8cf3d5c
Davide Tantillo <davide.tantillo@mia-platform.eu>
2022-04-16 11:29:08 +0200

    Passing a string to a function that want number

--- types/dist/index.js
+++ types/dist/index.js
@@ -1,1 +1,7 @@
-console.log("Hello, Typescript");
+function calculateTax(amount) {
+    return amount * 1.2;
+}
+const price = 12;
+console.log(`No TAX price: ${price} - TAX Price: ${calculateTax(price)}`);
+const stringPrice = "12";
+console.log(`No TAX price: ${stringPrice} - TAX stringPrice: ${calculateTax(stringPrice)}`);
--- types/src/index.ts
+++ types/src/index.ts
@@ -1,1 +1,9 @@
-console.log("Hello, Typescript")
+function calculateTax(amount: number): number {
+    return amount * 1.2
+}
+
+const price: number = 12
+console.log(`No TAX price: ${price} - TAX Price: ${calculateTax(price)}`)
+
+const stringPrice: string = "12"
+console.log(`No TAX price: ${stringPrice} - TAX stringPrice: ${calculateTax(stringPrice)}`)
