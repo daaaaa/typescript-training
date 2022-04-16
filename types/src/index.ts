805854a3ac257e79eb01fd354d09cdbbde3be8fd
Davide Tantillo <davide.tantillo@mia-platform.eu>
2022-04-16 16:30:53 +0200

    ts not allow to call a function with less args, neither more

--- types/src/index.ts
+++ types/src/index.ts
@@ -1,10 +1,8 @@
-function calculateTax(amount) {
-    return amount * 1.2;
-}
-
 function calculateTax(amount, discount) {
-    return calculateTax(amount) - discount
+    return (amount * 1.2) - discount
 }
 
-let taxValue = calculateTax(100);
-console.log(`Total Amount: ${taxValue}`);
+const taxValue = calculateTax(100, 0)
+console.log(`Total Amount: ${taxValue}`)
+
+const taxValueTwo = calculateTax(199)
