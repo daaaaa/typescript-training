d0bff9ca0e8fcd2de2d4baa3972daa9084755917
Davide Tantillo <davide.tantillo@mia-platform.eu>
2022-04-16 11:34:01 +0200

    Shows that implicit types - especially for function return values - can lead to misunderstandings

--- types/src/index.ts
+++ types/src/index.ts
@@ -1,9 +1,13 @@
-function calculateTax(amount: number): number {
-    return amount * 1.2
+function calculateTax(amount: number) {
+    return (amount * 1.2).toFixed(2)
 }
 
 const price: number = 12
 console.log(`No TAX price: ${price} - TAX Price: ${calculateTax(price)}`)
 
-const stringPrice = "12"
-console.log(`No TAX price: ${stringPrice} - TAX stringPrice: ${calculateTax(stringPrice)}`)
+// const stringPrice = "12"
+// console.log(`No TAX price: ${stringPrice} - TAX stringPrice: ${calculateTax(stringPrice)}`)
+
+const halfTotalPrice = calculateTax(price) / 2
+
+console.log(`The halved price is ${halfTotalPrice}`);
