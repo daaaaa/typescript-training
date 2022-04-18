c9b540935c246fedee27f7a26528ece222366a5e
Davide Tantillo <davide.tantillo@mia-platform.eu>
2022-04-18 12:32:45 +0200

    Object chapter starts: show how compiler in declaration file reuse the same type shape to define the array type

--- types/dist/index.d.ts
+++ types/dist/index.d.ts
@@ -1,35 +1,12 @@
-declare function calculateTax(amount: number): number;
-declare function writePrice(product: string, price: number): void;
-declare enum Product {
-    Hat = 0,
-    Gloves = 20,
-    Umbrella = 21
-}
-declare const productValue: Product;
-declare const unionValue: number | Product;
-declare enum City {
-    London = "London",
-    Paris = "Paris",
-    NY = "New York",
-    Gorgonzola = "Gorgonzola"
-}
-declare const enum Colors {
-    Red = 0,
-    Green = 1,
-    Blue = 2
-}
-declare const restrictedValue: 1 | 2 | 3;
-declare function calculatePrice(quantity: 1 | 2, price: number): number;
-declare const total: number;
-declare type numVals = 1 | 2 | 3 | 4;
-declare function getRandomValue(): numVals;
-declare function getMixedValue(input: 1): 1;
-declare function getMixedValue(input: 2 | 3): "Hello" | true;
-declare function getMixedValue(input: 4): City.London;
-declare const first: 1;
-declare const second: true | "Hello";
-declare const third: true | "Hello";
-declare type Cities = "London" | "Paris" | "Chicago";
-declare type CityResponse = `City: ${Cities}`;
-declare function getCityString(city: Cities): CityResponse;
-declare let stringCity: "City: London" | "City: Paris" | "City: Chicago";
+declare const hat: {
+    name: string;
+    price: number;
+};
+declare const gloves: {
+    name: string;
+    price: number;
+};
+declare const products: {
+    name: string;
+    price: number;
+}[];
