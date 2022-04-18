bbc5c1e5ea1e95283a0adb57ae7a877a32025bcf
Davide Tantillo <davide.tantillo@mia-platform.eu>
2022-04-18 12:39:03 +0200

    Add missing properties and an additional one to the umbrella object so that the compiler can accept the type. In Typescript additional properties can be specified, the important is that there are not missing ones

--- types/src/index.ts
+++ types/src/index.ts
@@ -1,6 +1,6 @@
 const hat = { name: "Hat", price: 100 }
 const gloves = { name: "Gloves", price: 75 }
-const umbrella = { name: "Umbrella" }
+const umbrella = { name: "Umbrella", price: 30, waterproof: true }
 const products: { name: string, price: number }[] = [hat, gloves, umbrella]
 
 products.forEach(prod => console.log(`${prod.name}: ${prod.price}`))
