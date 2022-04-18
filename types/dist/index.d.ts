4a778f727893ad1f81d7388ff47fc7495f51a130
Davide Tantillo <davide.tantillo@mia-platform.eu>
2022-04-18 10:54:28 +0200

    Add rest parameter type to tuple type. This is supported, but it loses some rigidity that make tuples more interesting.

--- types/dist/index.d.ts
+++ types/dist/index.d.ts
@@ -1,5 +1,5 @@
 declare function calculateTax(amount: number): number;
 declare function writePrice(product: string, price: number): void;
-declare const hat: [string, number, number?];
-declare const gloves: [string, number, number?];
-declare const items: [string, number, (number | undefined)?][];
+declare const hat: [string, number, number?, ...number[]];
+declare const gloves: [string, number, number?, ...number[]];
+declare const items: [string, number, (number | undefined)?, ...number[]][];
--- types/dist/index.js
+++ types/dist/index.js
@@ -4,13 +4,14 @@ function calculateTax(amount) {
 function writePrice(product, price) {
     console.log(`Price for ${product}: $${price.toFixed(2)}`);
 }
-const hat = ["Hat", 100];
+const hat = ["Hat", 100, 10, 1.20, 3, 0.95];
 const gloves = ["Gloves", 75, 10];
 const items = [hat, gloves];
 items.forEach((tuple) => {
-    let [name, price, taxRate] = tuple;
+    let [name, price, taxRate, ...coupons] = tuple;
     if (taxRate !== undefined) {
         price += price * (taxRate / 100);
     }
+    coupons.forEach(c => price -= c);
     writePrice(name, price);
 });
