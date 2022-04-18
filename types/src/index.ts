89cc696ffafaa4a978529f3cb4869a6d8a80200f
Davide Tantillo <davide.tantillo@mia-platform.eu>
2022-04-18 12:46:52 +0200

    Including methods in shape type and showing that even though it is optional and used js will return an error because undefined is invoked

--- types/src/index.ts
+++ types/src/index.ts
@@ -1,6 +1,16 @@
+const enum Feature { Waterproof, Insulated }
+
 const hat = { name: "Hat", price: 100 }
 const gloves = { name: "Gloves", price: 75 }
-const umbrella = { name: "Umbrella", price: 30, waterproof: true }
-const products: { name: string, price?: number, waterproof?: boolean }[] = [hat, gloves, umbrella]
+const umbrella = {
+    name: "Umbrella",
+    price: 30,
+    waterproof: true,
+    hasFeature: (feature: Feature) => feature === Feature.Waterproof
+}
+const products: {
+    name: string,
+    price?: number,
+    hasFeature?(Feature): boolean }[] = [hat, gloves, umbrella]
 
-products.forEach(prod => console.log(`${prod.name}: ${prod.price} - Waterproof: ${prod.waterproof}`))
+products.forEach(prod => console.log(`${prod.name}: ${prod.price} - Waterproof: ${prod.hasFeature(Feature.Waterproof)}`))
