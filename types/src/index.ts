01545ca8b7ca903a0a0c651c69053718c34579ca
Davide Tantillo <davide.tantillo@mia-platform.eu>
2022-04-18 12:37:17 +0200

    Use object shape type annotation. Now the problem is that umbrella does not fit the products type

--- types/src/index.ts
+++ types/src/index.ts
@@ -1,6 +1,6 @@
 const hat = { name: "Hat", price: 100 }
 const gloves = { name: "Gloves", price: 75 }
 const umbrella = { name: "Umbrella" }
-const products = [hat, gloves, umbrella]
+const products: { name: string, price: number }[] = [hat, gloves, umbrella]
 
 products.forEach(prod => console.log(`${prod.name}: ${prod.price}`))
