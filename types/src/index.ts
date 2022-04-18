445831bc271f28c6b9407d961c10b17263fb7525
Davide Tantillo <davide.tantillo@mia-platform.eu>
2022-04-18 13:01:27 +0200

    Explicitly creating the unnion type of Person and Product and showing how id is a union of the type of the id prop from both Person and Product

--- types/src/index.ts
+++ types/src/index.ts
@@ -12,11 +12,16 @@ type Person = {
     city: string,
 }
 
+type UnionType = {
+    id: number | string,
+    name: string,
+}
+
 const hat = { id: 1, name: "Hat", price: 100 }
 const gloves = { id: 2, name: "Gloves", price: 75 }
 const umbrella = { id: 3, name: "Umbrella", price: 30 }
 const bob = { id: "bsmith", name: "Umbrella", city: "London" }
 
-const dataItems: (Product | Person)[] = [hat, gloves, umbrella, bob]
+const dataItems: UnionType[] = [hat, gloves, umbrella, bob]
 
 dataItems.forEach(item => console.log(`ID: ${item.id}, ${item.name}`))
