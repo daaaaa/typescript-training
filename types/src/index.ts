45fb5a567a3d89a9240e621619fd133e27071576
Davide Tantillo <davide.tantillo@mia-platform.eu>
2022-04-18 12:53:55 +0200

    show how additional properties are not accepted for type shape defined using alias while they are accepted when using object literals

--- types/src/index.ts
+++ types/src/index.ts
@@ -14,6 +14,10 @@ const umbrella = {
     waterproof: true,
     hasFeature: (feature: Feature) => feature === Feature.Waterproof
 }
-const products: Product[] = [hat, gloves, umbrella]
+
+const mirrorShades = { name: "Sunglasses", price: 54, finish: "mirrored" }
+const darkShades: Product = { name: "Sunglasses", price: 54, finish: "mirrored" }
+
+const products: Product[] = [hat, gloves, umbrella, mirrorShades, darkShades]
 
 products.forEach(prod => console.log(`${prod.name}: ${prod.price} - Waterproof: ${prod.hasFeature ? prod.hasFeature(Feature.Waterproof) : false}`))
