a30514cfd822c1ba3000453982b47e6c3d95b5b7
Davide Tantillo <davide.tantillo@mia-platform.eu>
2022-04-20 21:55:53 +0200

    showing tha # symbol that will be added to js for private fields

--- types/dist/index.js
+++ types/dist/index.js
@@ -1,14 +1,30 @@
+var __classPrivateFieldSet = (this && this.__classPrivateFieldSet) || function (receiver, privateMap, value) {
+    if (!privateMap.has(receiver)) {
+        throw new TypeError("attempted to set private field on non-instance");
+    }
+    privateMap.set(receiver, value);
+    return value;
+};
+var __classPrivateFieldGet = (this && this.__classPrivateFieldGet) || function (receiver, privateMap) {
+    if (!privateMap.has(receiver)) {
+        throw new TypeError("attempted to get private field on non-instance");
+    }
+    return privateMap.get(receiver);
+};
+var _dept;
 class Employee {
     constructor(id, name, dept, city) {
+        _dept.set(this, void 0);
         this.id = id;
         this.name = name;
-        this.dept = dept;
+        __classPrivateFieldSet(this, _dept, dept);
         this.city = city;
     }
     writeDept() {
-        console.log(`${this.name} works in ${this.dept}`);
+        console.log(`${this.name} works in ${__classPrivateFieldGet(this, _dept)}`);
     }
 }
+_dept = new WeakMap();
 let salesEmployee = new Employee("fvega", "Fidel Vega", "Sales", "Paris");
 salesEmployee.writeDept();
 let data = [
