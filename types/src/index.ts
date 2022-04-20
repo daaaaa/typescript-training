6fc201ff84f2caa0eb1e247fc4e7f5f388aa1250
Davide Tantillo <davide.tantillo@mia-platform.eu>
2022-04-20 22:05:32 +0200

    introducing the extends keyword to show inheritance in ts

--- types/src/index.ts
+++ types/src/index.ts
@@ -1,17 +1,19 @@
-type Person = {
-    id: string,
-    name: string,
-    city: string
+class Person {
+    constructor(
+        public id: string,
+        public name: string,
+        public city: string
+    ) {}
 }
 
-class Employee {
-    
-
+class Employee extends Person {
     constructor(
         public readonly id: string,
         public name: string,
         private dept: string,
-        city: string) {}
+        public city: string) {
+            super(id, name, city)
+        }
 
     writeDept() {
         console.log(`${this.name} works in ${this.dept}`)
@@ -19,21 +21,18 @@ class Employee {
 }
  
 
-let salesEmployee = new Employee("fvega", "Fidel Vega", "Sales", "Paris")
+const salesEmployee = new Employee("fvega", "Fidel Vega", "Sales", "Paris")
 salesEmployee.writeDept()
 // salesEmployee.id = "fidel"
 
-let data: (Person | Employee)[] = [
-    { id: "bsmith", name: "Bob Smith", city: "London" },
-    { id: "ajones", name: "Alice Jones", city: "Paris"},
-    { id: "dpeters", name: "Dora Peters", city: "New York"},
+const data: (Person | Employee)[] = [
+    new Person("bsmith", "Bob Smith", "London"),
     salesEmployee,
 ]
 
 data.forEach(item => {
+    console.log(`${item.id} ${item.name}, ${item.city}`)
     if (item instanceof Employee) {
         item.writeDept()
-    } else {
-        console.log(`${item.id} ${item.name}, ${item.city}`)
     }
 })
