ec7a779868c85047ee090d338b138e5b518d5f03
Davide Tantillo <davide.tantillo@mia-platform.eu>
2022-04-20 21:56:54 +0200

    showing that targeting esNext js version the js produced file will keep the # keyword instead of replacing it with other kind of code

--- types/dist/index.js
+++ types/dist/index.js
@@ -1,30 +1,18 @@
-var __classPrivateFieldSet = (this && this.__classPrivateFieldSet) || function (receiver, privateMap, value) {
-    if (!privateMap.has(receiver)) {
-        throw new TypeError("attempted to set private field on non-instance");
-    }
-    privateMap.set(receiver, value);
-    return value;
-};
-var __classPrivateFieldGet = (this && this.__classPrivateFieldGet) || function (receiver, privateMap) {
-    if (!privateMap.has(receiver)) {
-        throw new TypeError("attempted to get private field on non-instance");
-    }
-    return privateMap.get(receiver);
-};
-var _dept;
 class Employee {
+    id;
+    name;
+    #dept;
+    city;
     constructor(id, name, dept, city) {
-        _dept.set(this, void 0);
         this.id = id;
         this.name = name;
-        __classPrivateFieldSet(this, _dept, dept);
+        this.#dept = dept;
         this.city = city;
     }
     writeDept() {
-        console.log(`${this.name} works in ${__classPrivateFieldGet(this, _dept)}`);
+        console.log(`${this.name} works in ${this.#dept}`);
     }
 }
-_dept = new WeakMap();
 let salesEmployee = new Employee("fvega", "Fidel Vega", "Sales", "Paris");
 salesEmployee.writeDept();
 let data = [
