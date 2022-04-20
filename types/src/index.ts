9565ee9393f6d68a5377f72701af06305468e7c3
Davide Tantillo <davide.tantillo@mia-platform.eu>
2022-04-20 22:01:45 +0200

    showing the short syntax for construct and instand field creation and the fact that it need the access control keyword. If no access keyword is given, the prop will no be saved as instance field

--- types/src/index.ts
+++ types/src/index.ts
@@ -5,27 +5,23 @@ type Person = {
 }
 
 class Employee {
-    public readonly id: string
-    public name: string
-    #dept: string
-    public city: string
+    
 
-    constructor(id: string, name: string, dept: string, city: string) {
-        this.id = id
-        this.name = name
-        this.#dept = dept
-        this.city = city
-    }
+    constructor(
+        public readonly id: string,
+        public name: string,
+        private dept: string,
+        city: string) {}
 
     writeDept() {
-        console.log(`${this.name} works in ${this.#dept}`)
+        console.log(`${this.name} works in ${this.dept}`)
     }
 }
  
 
 let salesEmployee = new Employee("fvega", "Fidel Vega", "Sales", "Paris")
 salesEmployee.writeDept()
-salesEmployee.id = "fidel"
+// salesEmployee.id = "fidel"
 
 let data: (Person | Employee)[] = [
     { id: "bsmith", name: "Bob Smith", city: "London" },
