ac9bef1c756256ef1506f340647c9f8f1f5b5735
Davide Tantillo <davide.tantillo@mia-platform.eu>
2022-04-20 21:52:03 +0200

    showing access control keyword and the fact that private props can only be accesed within the class context

--- types/src/index.ts
+++ types/src/index.ts
@@ -5,10 +5,10 @@ type Person = {
 }
 
 class Employee {
-    id: string
-    name: string
-    dept: string
-    city: string
+    public id: string
+    public name: string
+    private dept: string
+    public city: string
 
     constructor(id: string, name: string, dept: string, city: string) {
         this.id = id
@@ -24,6 +24,7 @@ class Employee {
  
 
 let salesEmployee = new Employee("fvega", "Fidel Vega", "Sales", "Paris")
+console.log(`Dept value: ${salesEmployee.dept}`)
 
 let data: (Person | Employee)[] = [
     { id: "bsmith", name: "Bob Smith", city: "London" },
