12284efb90e37e9782d6174a8fc4cd012286b1c4
Davide Tantillo <davide.tantillo@mia-platform.eu>
2022-04-23 20:31:37 +0200

    introducing conditional types. They can be use as a placeholder type for the type that will result after an evaluation of an expression

--- types/dist/index.js
+++ types/dist/index.js
@@ -1,39 +1,5 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-const p1 = { name: "Bob", city: "London" };
-const p2 = { name: "Alice", city: "Paris" };
-console.log(`Custom Mapped type: ${p1.name}, ${p2.city}`);
-console.log(`Built-in Record type: ${p2.name}, ${p2.city}`);
-// const e = new Employee("Bob Smith", "Sales")
-// const products = [
-//     p,
-//     new Product("Hat", 25),
-// ]
-// type shapeType = { name: string }
-// class Collection<T, K extends keyof T> implements Iterable<T> {
-//     private items: Map<T[K], T>
-//     constructor(private initialItems: T[] = [], private propertyName: K) {
-//         this.items = new Map<T[K], T>()
-//         this.add(...initialItems)
-//     }
-//     add(...newItems: T[]): void {
-//         newItems.forEach(newItem => this.items.set(newItem[this.propertyName], newItem))
-//     }
-//     get(key: T[K]): T {
-//         return this.items.get(key)
-//     }
-//     get count(): number {
-//         return this.items.size
-//     }
-//     [Symbol.iterator](): Iterator<T> {
-//         return this.items.values()
-//     }
-// }
-// const productCollection: Collection<Product, "name"> = new Collection(products, "name")
-// console.log(`There are ${ productCollection.count } products`)
-// const itemByKey = productCollection.get("Hat")
-// console.log(`Item: ${itemByKey.name}, ${itemByKey.price}`)
-// const productCollectionByPrice: Collection<Product, "price"> = new Collection(products, "price")
-// console.log(`There are ${ productCollectionByPrice.count } products`)
-// const itemByKeyPrice = productCollectionByPrice.get(100)
-// console.log(`Item: ${itemByKey.name}, ${itemByKey.price}`)
+const firstVal = "String Value";
+const secondVal = 100;
+const mismatchCheck = "String Value";
--- types/src/index.ts
+++ types/src/index.ts
@@ -1,55 +1,8 @@
 import { City, Person, Product, Employee } from "./dataTypes"
 
-type CustomMapped<K extends keyof any, T> = {
-    [P in K]: T
-}
+type resultType<T extends boolean> = T extends true ? string : number
 
+const firstVal: resultType<true> = "String Value"
+const secondVal: resultType<false> = 100
 
-const p1: CustomMapped<"name" | "city", string> = { name: "Bob", city: "London" }
-const p2: Record<"name" | "city", string> = { name: "Alice", city: "Paris" }
-console.log(`Custom Mapped type: ${p1.name}, ${p2.city}`)
-console.log(`Built-in Record type: ${p2.name}, ${p2.city}`)
-
-// const e = new Employee("Bob Smith", "Sales")
-// const products = [
-//     p,
-//     new Product("Hat", 25),
-// ]
-
-// type shapeType = { name: string }
-
-// class Collection<T, K extends keyof T> implements Iterable<T> {
-//     private items: Map<T[K], T>
-
-//     constructor(private initialItems: T[] = [], private propertyName: K) {
-//         this.items = new Map<T[K], T>()
-//         this.add(...initialItems)
-//     }
-
-//     add(...newItems: T[]): void {
-//         newItems.forEach(newItem => this.items.set(newItem[this.propertyName], newItem))
-//     }
-
-//     get(key: T[K]): T {
-//         return this.items.get(key)
-//     }
-
-//     get count(): number {
-//         return this.items.size
-//     }
-
-//     [Symbol.iterator](): Iterator<T> {
-//         return this.items.values()
-//     }
-// }
-// const productCollection: Collection<Product, "name"> = new Collection(products, "name")
-// console.log(`There are ${ productCollection.count } products`)
-
-// const itemByKey = productCollection.get("Hat")
-// console.log(`Item: ${itemByKey.name}, ${itemByKey.price}`)
-
-// const productCollectionByPrice: Collection<Product, "price"> = new Collection(products, "price")
-// console.log(`There are ${ productCollectionByPrice.count } products`)
-
-// const itemByKeyPrice = productCollectionByPrice.get(100)
-// console.log(`Item: ${itemByKey.name}, ${itemByKey.price}`)
+const mismatchCheck: resultType<false> = "String Value"
