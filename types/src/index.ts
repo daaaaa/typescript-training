15d42ec90bb579b23e0d58e5dcfe6d99f8f8910b
Davide Tantillo <davide.tantillo@mia-platform.eu>
2022-04-23 15:42:10 +0200

    showing how the index type query operator (keyof) can be used to useful contraint the domain of a generic type and so of the parameters

--- types/src/index.ts
+++ types/src/index.ts
@@ -1,11 +1,19 @@
 import { City, Person, Product, Employee } from "./dataTypes"
 
-let myVar: keyof Product = "name"
-myVar = "price"
-myVar = "someOtherName"
+function getValue<T, K extends keyof T>(item: T, keyname: K) {
+    console.log(`Value: ${item[keyname]}`);
+}
+
+const p = new Product("Running Shoes", 100)
+getValue(p, "name")
+getValue(p, "price")
+
+const e = new Employee("Bob Smith", "Sales")
+getValue(e, "name")
+getValue(e, "role")
 
 const products = [
-    new Product("Running Shoes", 100),
+    p,
     new Product("Hat", 25),
 ]
 
