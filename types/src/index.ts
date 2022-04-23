b9eaeb40a88d3533eee6ea9134762ebbb12af2a5
Davide Tantillo <davide.tantillo@mia-platform.eu>
2022-04-23 15:18:37 +0200

    showing that js collection can be combined with generic type parameters. Showin the set

--- types/src/index.ts
+++ types/src/index.ts
@@ -8,20 +8,22 @@ const products = [
 type shapeType = { name: string }
 
 class Collection<T extends shapeType> {
-    constructor(
-        private items: T[] = []
-    ) {}
+    private items: Set<T>
+
+    constructor(private initialItems: T[] = []) {
+        this.items = new Set<T>(initialItems)
+    }
 
     add(...newItems: T[]): void {
-        this.items.push(...newItems)
+        newItems.forEach(newItem => this.items.add(newItem))
     }
 
     get(name: string): T {
-        return this.items.find(item => item.name === name)
+        return [...this.items.values()].find(item => item.name === name)
     }
 
     get count(): number {
-        return this.items.length
+        return this.items.size
     }
 }
 const productCollection: Collection<Product> = new Collection(products)
