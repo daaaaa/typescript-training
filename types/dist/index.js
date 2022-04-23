fb24092e688479ad5319da1ed4600351a5e887e9
Davide Tantillo <davide.tantillo@mia-platform.eu>
2022-04-23 15:06:49 +0200

    started chapter on advanced generic type

--- types/dist/index.js
+++ types/dist/index.js
@@ -1,29 +1,25 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const dataTypes_1 = require("./dataTypes");
-class ArrayCollection {
-    constructor() {
-        this.items = [];
+const products = [
+    new dataTypes_1.Product("Running Shoes", 100),
+    new dataTypes_1.Product("Hat", 25),
+];
+class Collection {
+    constructor(items = []) {
+        this.items = items;
     }
     add(...newItems) {
         this.items.push(...newItems);
+    }
+    get(name) {
+        return this.items.find(item => item.name === name);
     }
     get count() {
         return this.items.length;
     }
 }
-class ProductCollection extends ArrayCollection {
-    get(searchTerm) {
-        return this.items.find(item => item.name === searchTerm);
-    }
-}
-class PersonCollection extends ArrayCollection {
-    get(searchTerm) {
-        return this.items.find(item => item.name === searchTerm || item.city === searchTerm);
-    }
-}
-const peopleCollection = new PersonCollection();
-peopleCollection.add(new dataTypes_1.Person("Bob Smith", "London"), new dataTypes_1.Person("Dora Peters", "New York"));
-const productCollection = new ProductCollection();
-productCollection.add(new dataTypes_1.Product("Running shoes", 100), new dataTypes_1.Product("Hat", 25));
-[peopleCollection, productCollection].forEach(c => console.log(`Collection size: ${peopleCollection.count}`));
+const productCollection = new Collection(products);
+console.log(`There are ${productCollection.count} products`);
+const p = productCollection.get("Hat");
+console.log(`Product: ${p.name}, ${p.price}`);
