95b52ddfb7a902ae5d6ee29917a4ec2a06f937d8
Davide Tantillo <davide.tantillo@mia-platform.eu>
2022-04-24 17:07:25 +0200

    showing the usage of a 4rd part library with declaration files included (chalk) and that compiler is able to use them

--- usingjs/src/index.ts
+++ usingjs/src/index.ts
@@ -1,7 +1,8 @@
 import debug = require('debug');
+import chalk = require('chalk');
 import { SportsProduct, SPORT } from './product';
 import Cart from './cart';
-import { sizeFormatter, costFormatter, writeMessage } from './formatters';
+import { sizeFormatter, costFormatter } from './formatters';
 
 const kayak = new SportsProduct(1, 'Kayak', 275, SPORT.Watersports);
 const hat = new SportsProduct(2, 'Hat', 22.10, SPORT.Watersports);
@@ -15,6 +16,9 @@ cart.addProduct(hat, 2);
 sizeFormatter('Cart', cart.itemCount);
 costFormatter('Cart', cart.totalPrice);
 
-const db = debug('Example App', true);
+const db = debug('Example App');
 db.enabled = true;
 db('message: %o', 'Test message');
+
+console.log(chalk.greenBright('Formatted message'));
+console.log(chalk.notAColor('Formatted message'));
