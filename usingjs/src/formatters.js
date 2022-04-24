914ba890e81fc1dc81f69dcaa9ca19a09ade284c
Davide Tantillo <davide.tantillo@mia-platform.eu>
2022-04-24 16:03:49 +0200

    adding a js file in src and showing that ts locates the js file, but it is not copied in the dist folder and node do not find it

---
+++ usingjs/src/formatters.js
@@ -0,0 +1,11 @@
+function writeMessage(message) {
+    console.log(message);
+}
+
+export function sizeFormatter(thing, count) {
+    writeMessage(`The ${thing} has ${count} items`);
+}
+
+export function costFormatter(thing, cost) {
+    writeMessage(`The ${thing} costs $${cost.toFixed(2)} items`, true);
+}
