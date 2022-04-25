b3e64515cd8c95570757823e602ebd333f350ed7
Davide Tantillo <davide.tantillo@mia-platform.eu>
2022-04-25 21:22:36 +0200

    Initialized webapp_plain for the new chapter (15-16)

---
+++ webapp_plain/dist/bundle.js
@@ -0,0 +1,9 @@
+/******/ (() => { // webpackBootstrap
+/*!**********************!*\
+  !*** ./src/index.ts ***!
+  \**********************/
+console.log('Web App');
+
+/******/ })()
+;
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbIndlYnBhY2s6Ly93ZWJhcHBfcGxhaW4vLi9zcmMvaW5kZXgudHMiXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6Ijs7OztBQUFBIiwiZmlsZSI6ImJ1bmRsZS5qcyIsInNvdXJjZXNDb250ZW50IjpbImNvbnNvbGUubG9nKCdXZWIgQXBwJyk7XG4iXSwic291cmNlUm9vdCI6IiJ9
