efd7d80854ba54e4e8a4ca0f98c798485dad5cc8
Davide Tantillo <davide.tantillo@mia-platform.eu>
2022-04-25 22:22:14 +0200

    Added data stores that will be used later

---
+++ webapp_plain/src/data/abstractDataSource.ts
@@ -0,0 +1,52 @@
+import {Product, Order} from './entities';
+
+export type ProductProp = keyof Product
+
+export abstract class AbstractDataSource {
+    private products: Product[];
+    private categories: Set<string>;
+    public order: Order;
+    public loading: Promise<void>;
+
+    constructor() {
+        this.products = [];
+        this.categories = new Set<string>();
+        this.order = new Order();
+        this.loading = this.getData();
+    }
+
+    async getProducts(sortProp: ProductProp = 'id', category?: string): Promise<Product[]> {
+        await this.loading;
+        return this.selectProducts(this.products, sortProp, category);
+    }
+
+    protected async getData(): Promise<void> {
+        this.products = [];
+        this.categories.clear();
+        const rawData = await this.loadProducts();
+        rawData.forEach(product => {
+            this.products.push(product);
+            this.categories.add(product.category);
+        });
+    }
+
+    protected selectProducts(
+        prods: Product[],
+        sortProp: ProductProp,
+        category?: string,
+    ): Product[] {
+        return prods
+            .filter(p => category === undefined || p.category === category)
+            .sort((p1, p2) => p1[sortProp] < p2[sortProp]
+                ? -1
+                : p1[sortProp] > p2[sortProp] ? 1 : 0);
+    }
+
+    async getCategories(): Promise<string[]> {
+        await this.loading;
+        return [...this.categories.values()];
+    }
+
+    protected abstract loadProducts(): Promise<Product[]>;
+    abstract storeOrder(): Promise<number>;
+}
--- webapp_plain/src/index.ts
+++ webapp_plain/src/index.ts
@@ -1,2 +1,22 @@
-console.log('Web App');
-console.log('New statement');
+import {LocalDataSource} from './data/localDataSource';
+
+async function displayData(): Promise<string> {
+    const ds = new LocalDataSource();
+    const allProducts = await ds.getProducts('name');
+    const categories = await ds.getCategories();
+    const chessProducts = await ds.getProducts('name', 'Chess');
+
+    let result = '';
+    allProducts.forEach(product => {
+        result += `Product: ${product.name}, ${product.category}\n`;
+    });
+    categories.forEach(category => {
+        result += `Category: ${category}\n`;
+    });
+    chessProducts.forEach(product => ds.order.addProduct(product, 1));
+    result += `Order total: ($${ds.order.total.toFixed(2)})`;
+    return result;
+}
+
+displayData()
+    .then(result => console.log(result));
